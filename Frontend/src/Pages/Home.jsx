e5f32b3bc8c8073df8c67a441d29a44358fd1f2a
Ravindra Prajapat <75526037+ravindrakumardev@users.noreply.github.com>
2026-02-18 17:56:21 +0530

    fix

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Home() {
   return (
     <div>
-      <h1>Welcome to my React Homepage!</h1>
+      <h1 className='text-blue-900'>Welcome to my React Homepage!</h1>
       <p>This is the main landing page.</p>
     </div>
   );
